1441a3114e7822a2a16cd753244249a313940b12
ivanlie <ivan.lie@meditap.id>
2026-04-07 17:26:36 +0700

    Initial Commit

---
+++ Test Mobile iOS/Test_Mobile_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_Mobile_iOSApp.swift
+//  Test Mobile iOS
+//
+//  Created by Ivan Lie on 07/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Test_Mobile_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
